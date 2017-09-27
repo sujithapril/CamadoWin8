af935a02ae025f6e252b58fa32fcfbbddd92342a
Priyanka Arunraj <pvpriya7@gmail.com>
2017-09-27 14:32:19 +0530

    adding messageclass for graph load communication

---
+++ CamadoWin8.Shared/LoadGroupedBarGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadoWin8.Shared
+{
+    public class LoadGroupedBarGraph
+    {
+        public string Message { get; set; }
+
+    }
+}
